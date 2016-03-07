112f82d530905a715582ff0eb71259aaf5108f8a
l1f3h4ck <i.indrajith.143@gmail.com>
2016-03-08 00:51:35 +0530

    integrated db

--- pickle.py
+++ pickle.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, render_template
 from flask import request
 from flask import make_response
 from flask import redirect
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
@@ -11,28 +12,30 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.sqlalchemy import SQLAlchemy
-
+#from flask.ext.migrate import Migrate, MirateCommand
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+test = "Success"
 app =  Flask(__name__)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMIT_ON_TEARDOWN'] = True
 
-db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
-
 moment = Moment(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+#migrate = Migrate(app,db)
+#manager.add_command('db', MigrateCommand)
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Colum(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -41,7 +44,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -51,12 +54,25 @@ class NameForm(Form):
     feel = StringField('How are you feeling today?')
     submit = SubmitField('Submit')
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():    
     name = None
     feel = None
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            flash('you have added a new user.')
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        
         old_name =  session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
@@ -67,8 +83,8 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', 
         form=form, name=session.get('name'),
-        feel=session.get('feel'), current_time=datetime.utcnow())
-
+        feel=session.get('feel'), current_time=datetime.utcnow(),
+        known = session.get('known', False))
 @app.route('/whoami')
 def whoami():
     user_agent = request.headers.get('User-Agent')
@@ -87,11 +103,17 @@ def page_not_found(e):
 
   
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    db.create_all()
+#    app.run(debug=True)
 
 #if __name__ == '__main__':
+#    db.create_all()
 #    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+#    db.create_all()
+    manager.run()
+
 
 
 
