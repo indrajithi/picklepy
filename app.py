ddcf93791632b9dffb32141b7df741e4192cb1b3
l1f3h4ck <i.indrajith.143@gmail.com>
2016-03-07 22:50:50 +0530

    db

--- app.py
+++ app.py
@@ -9,13 +9,40 @@ from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app =  Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 moment = Moment(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Colum(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ == 'users'
+    id = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
 
 class NameForm(Form):
     name = StringField('What is your name?', validators = [Required()])
@@ -28,12 +55,17 @@ def index():
     feel = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name =  session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-       # form.name.data = ''
+        form.name.data = ''
         session['feel'] = form.feel.data
-       # form.feel.data = ''
+        form.feel.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),feel=session.get('feel'), current_time=datetime.utcnow())
+    return render_template('index.html', 
+        form=form, name=session.get('name'),
+        feel=session.get('feel'), current_time=datetime.utcnow())
 
 @app.route('/whoami')
 def whoami():
